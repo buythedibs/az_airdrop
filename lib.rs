5f584b663f8d121c0ad9554a67d5f06c7c5ba0fc
Steven Chang <stevenchang5000@protonmail.com>
2024-02-15 14:51:15 +1000

    Fn config (#1)
    * fn config
    
    * fn config

--- lib.rs
+++ lib.rs
@@ -2,141 +2,66 @@
 
 #[ink::contract]
 mod az_airdrop {
+    // === STRUCTS ===
+    #[derive(Debug, Clone, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub struct Config {
+        admin: AccountId,
+        start: Timestamp,
+    }
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    // === CONTRACT ===
     #[ink(storage)]
     pub struct AzAirdrop {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        admin: AccountId,
+        start: Timestamp,
     }
-
     impl AzAirdrop {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(start: Timestamp) -> Self {
+            Self {
+                admin: Self::env().caller(),
+                start,
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+        // === QUERIES ===
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn config(&self) -> Config {
+            Config {
+                admin: self.admin,
+                start: self.start,
+            }
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
+        use ink::env::{
+            test::{default_accounts, set_caller, DefaultAccounts},
+            DefaultEnvironment,
+        };
 
-        /// We test if the default constructor does its job.
+        const MOCK_START: Timestamp = 654_654;
+
+        // === HELPERS ===
+        fn init() -> (DefaultAccounts<DefaultEnvironment>, AzAirdrop) {
+            let accounts = default_accounts();
+            set_caller::<DefaultEnvironment>(accounts.bob);
+            let az_airdrop = AzAirdrop::new(MOCK_START);
+            (accounts, az_airdrop)
+        }
+
+        // === TESTS ===
+        // === TEST QUERIES ===
         #[ink::test]
-        fn default_works() {
-            let az_airdrop = AzAirdrop::default();
-            assert_eq!(az_airdrop.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut az_airdrop = AzAirdrop::new(false);
-            assert_eq!(az_airdrop.get(), false);
-            az_airdrop.flip();
-            assert_eq!(az_airdrop.get(), true);
-        }
-    }
-
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
-
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = AzAirdropRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("az_airdrop", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<AzAirdropRef>(contract_account_id.clone())
-                .call(|az_airdrop| az_airdrop.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            Ok(())
-        }
-
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = AzAirdropRef::new(false);
-            let contract_account_id = client
-                .instantiate("az_airdrop", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            let get = build_message::<AzAirdropRef>(contract_account_id.clone())
-                .call(|az_airdrop| az_airdrop.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = build_message::<AzAirdropRef>(contract_account_id.clone())
-                .call(|az_airdrop| az_airdrop.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<AzAirdropRef>(contract_account_id.clone())
-                .call(|az_airdrop| az_airdrop.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
-            Ok(())
+        fn test_config() {
+            let (accounts, az_airdrop) = init();
+            let config = az_airdrop.config();
+            // * it returns the config
+            assert_eq!(config.admin, accounts.bob);
+            assert_eq!(config.start, MOCK_START);
         }
     }
 }
