9942ddae0d7e86e08db557886a5a9cf819e52f8a
Steven Chang <stevenchang5000@protonmail.com>
2024-02-15 14:50:09 +1000

    fn config

--- lib.rs
+++ lib.rs
@@ -7,25 +7,31 @@ mod az_airdrop {
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub struct Config {
         admin: AccountId,
+        start: Timestamp,
     }
 
     // === CONTRACT ===
     #[ink(storage)]
     pub struct AzAirdrop {
         admin: AccountId,
+        start: Timestamp,
     }
     impl AzAirdrop {
         #[ink(constructor)]
-        pub fn new() -> Self {
+        pub fn new(start: Timestamp) -> Self {
             Self {
                 admin: Self::env().caller(),
+                start,
             }
         }
 
         // === QUERIES ===
         #[ink(message)]
         pub fn config(&self) -> Config {
-            Config { admin: self.admin }
+            Config {
+                admin: self.admin,
+                start: self.start,
+            }
         }
     }
 
@@ -37,11 +43,13 @@ mod az_airdrop {
             DefaultEnvironment,
         };
 
+        const MOCK_START: Timestamp = 654_654;
+
         // === HELPERS ===
         fn init() -> (DefaultAccounts<DefaultEnvironment>, AzAirdrop) {
             let accounts = default_accounts();
             set_caller::<DefaultEnvironment>(accounts.bob);
-            let az_airdrop = AzAirdrop::new();
+            let az_airdrop = AzAirdrop::new(MOCK_START);
             (accounts, az_airdrop)
         }
 
@@ -53,6 +61,7 @@ mod az_airdrop {
             let config = az_airdrop.config();
             // * it returns the config
             assert_eq!(config.admin, accounts.bob);
+            assert_eq!(config.start, MOCK_START);
         }
     }
 }
